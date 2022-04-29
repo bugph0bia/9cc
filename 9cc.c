f1b2e60d2c7c1d5042ace8dc765167687a9f9940
bugph0bia <64964079+bugph0bia@users.noreply.github.com>
2022-04-29 13:51:25 +0900

    step4: エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -21,6 +21,8 @@ struct Token {
     char *str;      // トークン文字列
 };
 
+// 入力プログラム
+char *user_input;
 // 現在着目してるトークン
 Token *token;
 
@@ -29,6 +31,20 @@ Token *token;
 void error(char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");  // pos 個の空白を出力
+    fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -47,7 +63,7 @@ bool consume(char op) {
 // それ以外の場合にはエラーを報告する。
 void expect(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     token = token->next;
 }
 
@@ -55,7 +71,7 @@ void expect(char op) {
 // それ以外の場合にはエラーを報告する。
 int expect_number() {
     if (token->kind != TK_NUM)
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     int val = token->val;
     token = token->next;
     return val;
